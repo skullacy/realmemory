5958794552e11ca8e37af25ac9d9e0a533dab41e
skullacy <skullacy@gmail.com>
2013-09-28 17:15:04 +0900

    Signed-off-by: skullacy <skullacy@gmail.com>

--- realmemory/src/com/example/realmemory/CommServerJson.java
+++ realmemory/src/com/example/realmemory/CommServerJson.java
@@ -1,9 +1,22 @@
 package com.example.realmemory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CommServerJson {
 	private static String serverUrl;
@@ -19,16 +32,61 @@ public class CommServerJson {
 	public void setParam(String query, String value){
 		params.add(new BasicNameValuePair(query, value));
 	}
-	public String getServerUrlWithQueryString(){
-		return null;
-	}
 	
-	
-	
+	//constructor
 	CommServerJson(){
 		if(getServerUrl() == null){
 			setServerUrl("http://skullacytest.cafe24.com");
 		}
+	}
+	
+	public String getData() throws UnsupportedEncodingException
+	{
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = new BufferedReader(new InputStreamReader(getInputStreamFromUrl(serverUrl, params), "utf-8"));
+		
+		try{
+			String line = null;
+			
+			while((line = br.readLine()) != null){
+				sb.append(line);
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return sb.toString();
+	}
+	
+	public JSONObject getJSONData() throws JSONException{
+		JSONObject jsonobj = null;
+		try {
+			String content = getData();
+			jsonobj = new JSONObject(content);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return jsonobj;
+		
+	}
+	
+	private static InputStream getInputStreamFromUrl(String url, List<NameValuePair> params){
+		InputStream contentStream = null;
+		try{
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+			UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+			post.setEntity(ent);
+			HttpResponse responsePost = httpclient.execute(post);
+			
+			contentStream = responsePost.getEntity().getContent();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return contentStream;
 	}
 	
 	
